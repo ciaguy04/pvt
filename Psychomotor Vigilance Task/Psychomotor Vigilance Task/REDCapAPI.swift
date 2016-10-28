be5eacf1062b7db89e617c6fd92bc36ddcb1c5f2
Emily Anthony <bombesin1@lowell-anthonys-macbook.local>
2016-10-28 16:56:01 -0500

    Static Dummy Data successfully Imported into REDCap.  Added SwiftyJSON and Alamofire to assist with Apple's poorly designed support for this functionality.

---
+++ Psychomotor Vigilance Task/Psychomotor Vigilance Task/REDCapAPI.swift
@@ -0,0 +1,56 @@
+//
+//  REDCapAPI.swift
+//  Psychomotor Vigilance Task
+//
+//  Created by Emily Anthony on 10/27/16.
+//  Copyright © 2016 vu_sleep. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+func getURL (_ url:String){
+    Alamofire.request(url).responseJSON { response in
+        print("request: \(response.request)")
+        print("response: \(response.response)")
+        print("data: \(response.data)")
+        print("result: \(response.result)")
+        
+        if let JSON = response.result.value {
+            print("JSON: \(JSON)")
+        }
+        
+    }
+}
+
+func postToURL (_ url:String){
+    
+    let data : [String: Any] = ["record": "1,2", "field_name":"pvt_data", "value":[1,2,3,4], "redcap_event_name": "post_night_2_pvt_arm_1"]
+    let jsonString = "[ \(JSON(data))" + "]"
+    print(jsonString)
+    
+    let parameters: Parameters =
+        
+    [   "token": TOKEN,
+        "content": "record",
+        "format": "json",
+        "type": "eav",
+        "overwriteBehavior": "normal",
+        "data": jsonString,
+        "returnContent": "count",
+        "returnFormat": "json"
+    ]
+    
+    Alamofire.request(url , method: .post, parameters: parameters, encoding: URLEncoding.default).responseJSON { response in
+        
+        print("request: \(response.request)")
+        print("response: \(response.response)")
+        print("data: \(response.data)")
+        print("result: \(response.result)")
+        
+        if let JSON = response.result.value {
+            print("JSON: \(JSON)")
+        }
+    }
+}
